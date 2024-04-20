75c5a04edfe423b58f2512b71eec071cb6e097fd
nessrinechebli <cheblinessrine22@gmail.com>
2024-04-20 12:26:00 +0100

    'comit4'

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Contact
 # Register your models here.
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display=('listing','customer','contact_date','subject') #ðli ybanou godamou
+    list_display_links=('listing','customer')# lien
+    search_fields=('subject',)#bar de recherche
+    list_filter=('contact_date',)
+
+admin.site.register(Contact,ContactAdmin)
