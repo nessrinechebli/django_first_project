8cb46e09c369b2bf7b4513a93fb80f7c27c12093
nessrinechebli <cheblinessrine22@gmail.com>
2024-04-20 12:14:58 +0100

    'comit3'

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from acount.models import CustomerProfile
-
+import smtplib
 # Create your views here.
+def send_email(subject, message, receiver_address="yawapen977@acentni.com"):
+    # Your email address and password
+    sender_address = "meriemmeriem19alg@gmail.com"
+    sender_password = "kdza nxxy ywus oyyc"
+
+
+    # Set up the subject and body of the email
+    header = f"\r\nSubject: {subject}"
+    body = f"\r\n{message}"
+
+    # Combine the headers and the body
+    combined = header + body
+
+    # Send the email!
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(sender_address, sender_password)
+    server.sendmail(sender_address, receiver_address, combined)
+    print("Email sent!")
+
 def contact(request):
     if request.method == "POST":
           listing=request.method == "GET"
